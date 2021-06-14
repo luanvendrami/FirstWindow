2eed002322ce5d1e65295f1ff7bc4b8293cc3f5d
luanvendrami <luan.vendrami@hotmail.com>
2021-06-14 00:09:57 -0300

    First window for registration models

---
+++ FirstWindow/Controllers/CadastroController.cs
@@ -0,0 +1,28 @@
+﻿using FirstWindow.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWindow.Controllers
+{
+    public class CadastroController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Cadastro cadastro)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(cadastro);
+        }
+    }
+}
---
+++ FirstWindow/Models/Cadastro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWindow.Models
+{
+    public class Cadastro
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="O campo Nome é necessário.")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage ="Nome deve conter de 3 à 20 caracteres.")]
+        [RegularExpression(@"^[a-zA-Z\u00C0-\u00FF""'\w-]*$", ErrorMessage ="Caracter inválido.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo Sobrenome é necessário.")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Sobrenome deve conter de 3 à 20 caracteres.")]
+        [RegularExpression(@"^[a-zA-Z\u00C0-\u00FF""'\w-]*$", ErrorMessage ="Caracter inválido.")]
+        public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "O campo Data é necessário.")]
+        [DataType(DataType.DateTime, ErrorMessage ="Data em formato inválido.")]
+        [Display(Name ="Data de Cadastro")]
+        public DateTime DataAtual { get; set; }
+        
+        [Required(ErrorMessage = "O campo País é necessário.")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "País deve conter de 3 à 20 caracteres.")]
+        [RegularExpression(@"^[a-zA-Z\u00C0-\u00FF""'\w-]*$", ErrorMessage = "Caracter inválido.")]
+        public string  Pais { get; set; }
+    }
+}
